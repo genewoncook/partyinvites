37b6a8ec4a7133b1afc7b59d6ac8597387fa0238
genewoncook <genewoncook@gmail.com>
2015-12-08 12:02:29 -0600

    Initial put

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//My includes
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewData["greeting"] = (hour < 12 ? "Good morning" : "Good afternoon");
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            //return new EmptyResult();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            //TODO: Email
+            if (ModelState.IsValid)
+                return View("Thanks", guestResponse);
+            else // There's a validation error so redisplay the form
+                return View();
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Required Field")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "That is not a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required Field")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage ="Are you planning on attending?")]
+        public bool? WillAttend { get; set; }
+    }
+}
