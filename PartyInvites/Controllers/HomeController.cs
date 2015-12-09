cf28bbef6be51ca4aa026ab189eb23ce73f0a048
genewoncook <genewoncook@gmail.com>
2015-12-08 21:16:14 -0600

    Added file upload capabilities

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -26,6 +26,12 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
         [HttpPost]
         public ActionResult RsvpForm(GuestResponse guestResponse)
         {
@@ -34,6 +40,35 @@ namespace PartyInvites.Controllers
                 return View("Thanks", guestResponse);
             else // There's a validation error so redisplay the form
                 return View();
+        }
+
+        /// <summary>
+        /// Post method for uploading files
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            try
+            {
+                /*Geting the file name*/
+                string filename = System.IO.Path.GetFileName(file.FileName);
+                /*Saving the file in server folder*/
+                file.SaveAs(Server.MapPath("~/Images/" + filename));
+                string filepathtosave = "Images/" + filename;
+                /*
+                 * HERE WILL BE YOUR CODE TO SAVE THE FILE DETAIL IN DATA BASE
+                 *
+                 */
+
+                ViewBag.Message = "File Uploaded successfully.";
+            }
+            catch
+            {
+                ViewBag.Message = "Error while uploading the files.";
+            }
+            return View();
         }
     }
 }
